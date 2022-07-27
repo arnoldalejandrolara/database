ecb837f36779777f08b3f047e6a167019e48919f
arnoldlara <97060311+arnoldalejandrolara@users.noreply.github.com>
2022-07-26 19:26:10 -0500

    actualizarfinal

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con  = mysqli_connect('localhost','root','','datatable_db');
+$con  = mysqli_connect('localhost','id19329624_root','S20/F7rW?hlyQiWi','id19329624_datatable_db');
 if(mysqli_connect_errno())
 {
     echo 'Database Connection Error';
